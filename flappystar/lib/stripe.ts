258e48a82f2ad7372f0863a725976b1253319d24
zr069 <niksarafi@icloud.com>
2026-03-20 18:41:28 +0100

    Enable all payment methods - PayPal Apple Pay Google Pay

--- flappystar/lib/stripe.ts
+++ flappystar/lib/stripe.ts
@@ -66,7 +66,7 @@ export async function createCheckoutSession(
 
   const session = await stripe.checkout.sessions.create({
     mode: 'payment',
-    payment_method_types: ['card'],
+    automatic_payment_methods: { enabled: true },
     line_items: [
       {
         price_data: {
